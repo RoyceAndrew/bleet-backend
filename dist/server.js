cbcb76220dccf586e781a1f2583658dbf0498a97
RoyceAndrew <royceandrew142@gmail.com>
2025-04-09 17:22:21 +0700

    fix: change cors

--- dist/server.js
+++ dist/server.js
@@ -9,7 +9,7 @@ env.config();
 const app = express();
 const port = process.env.PORT || 3000;
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://bleet-frontend-4bk4.vercel.app",
     optionsSuccessStatus: 200,
     credentials: true
 };
