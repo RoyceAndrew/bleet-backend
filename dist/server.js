afd0d6857a2fe938f658be0aa41da6312b758c7b
RoyceAndrew <royceandrew142@gmail.com>
2025-05-31 14:17:16 +0700

    fix: cors option

--- dist/server.js
+++ dist/server.js
@@ -9,7 +9,7 @@ env.config();
 const app = express();
 const port = process.env.PORT || 3000;
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     optionsSuccessStatus: 200,
     credentials: true
 };
